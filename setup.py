ad5b779140afab81ad4ccef9aa675e3ed8aaa4a2
Markus Wissinger <markus.wissinger@gmail.com>
2016-05-13 10:22:08 +0100

    update setup.py

--- setup.py
+++ setup.py
@@ -39,7 +39,7 @@ setup(
         'Topic :: Software Development',
         'License :: OSI Approved :: Apache Software License',
         'Programming Language :: Python :: 2.7',
-        # 'Programming Language :: Python :: 3.5',
+        'Programming Language :: Python :: 3.4',
     ],
     keywords='development tool type hinting unittest living documentation',
     packages=['ducktest'],
