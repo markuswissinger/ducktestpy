092ee4bad9e4f7ef817810d2d4e26890ed69612f
Markus Wissinger <markus.wissinger@gmail.com>
2016-07-11 17:00:56 +0100

    conditional processor

--- ducktest/type_wrappers.py
+++ ducktest/type_wrappers.py
@@ -1,18 +1,74 @@
-from collections import namedtuple
+from abc import abstractmethod, ABCMeta
+from collections import namedtuple, Container, Iterable, Mapping
+
+import mock
+from future.utils import iteritems
+
+from ducktest.another_typer import Processor
 
 
-class TypeWrapper(object):
-    def __init__(self, own_type):
-        self.own_type = own_type
-
-    @staticmethod
-    def from_parameter(get_type, parameter):
-        return TypeWrapper(get_type(parameter))
-
-
-class ContainerTypeWrapper(TypeWrapper):
-    def __init__(self, own_type, contained_types):
-        super(ContainerTypeWrapper, self).__init__(own_type)
-        self.contained_types = contained_types
+def get_plain_type(parameter):
+    if isinstance(parameter, mock.Mock):
+        try:
+            return parameter._spec_class
+        except AttributeError:
+            raise ValueError('Mock has no _spec_class')
+    else:
+        return type(parameter)
 
 
+PlainTypeWrapper = namedtuple('PlainTypeWrapper', 'own_type')
+ContainerTypeWrapper = namedtuple('ContainerTypeWrapper', ['own_type', 'contained_types'])
+MappingTypeWrapper = namedtuple('MappingTypeWrapper', ['own_type', 'mapped_types'])
+
+
+class ConditionalProcessor(Processor):
+    __metaclass__ = ABCMeta
+
+    def process(self, *args, **kwargs):
+        if self._condition(*args, **kwargs):
+            self._process(*args, **kwargs)
+        else:
+            self.next_processor.process(*args, **kwargs)
+
+    @abstractmethod
+    def _condition(self, *args, **kwargs):
+        pass
+
+    @abstractmethod
+    def _process(self, *args, **kwargs):
+        pass
+
+
+class MappingTypeProcessor(ConditionalProcessor):
+    def __init__(self, head):
+        super(MappingTypeProcessor, self).__init__()
+        self._get_type = head.process
+
+    def _process(self, mapping):
+        mapped_types = set([(self._get_type(key), self._get_type(value)) for key, value in iteritems(mapping)])
+        return MappingTypeWrapper(get_plain_type(mapping), mapped_types)
+
+
+class ContainerTypeProcessor(ConditionalProcessor):
+    def __init__(self, head):
+        super(ContainerTypeProcessor, self).__init__()
+        self._get_type = head.process
+
+    def _condition(self, container):
+        return all([
+            isinstance(container, Container),
+            isinstance(container, Iterable),
+            not isinstance(container, basestring),
+            not isinstance(container, Mapping)
+        ])
+
+    def _process(self, container):
+        contained_types = set([self._get_type(contained_types) for contained_types in container])
+        return ContainerTypeWrapper(get_plain_type(container), contained_types)
+
+
+class PlainTypeProcessor(Processor):
+    def process(self, value):
+        """implicit end of chain"""
+        return PlainTypeWrapper(get_plain_type(value))
