38d5458f9c41ba09fef5aa9d93d3d6320f0259f4
Markus Wissinger <markus.wissinger@gmail.com>
2016-07-19 16:12:58 +0100

    test docstring parser

---
+++ tests/parse_example.py
@@ -0,0 +1,27 @@
+"""uninteresting"""
+
+
+class SomeClass(object):
+    def a(self):
+        """"""
+        pass
+
+    def b(self):
+        pass
+
+    @classmethod
+    def c(self):
+        """one liner"""
+        pass
+
+    def d(self):
+        """two
+        liner"""
+
+    def e(self):
+        """
+        three liner
+        """
+
+    def f(self):
+        """with newline \n, or at the end \n"""
