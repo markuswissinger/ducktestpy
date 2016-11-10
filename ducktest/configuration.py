ac0e972e5739c67fe0703cb05bd4243da656826a
Markus Wissinger <markus.wissinger@gmail.com>
2016-11-10 07:32:40 +0000

    start python api

--- ducktest/configuration.py
+++ ducktest/configuration.py
@@ -7,14 +7,30 @@ class DucktestConfiguration(object):
             top_level_directory='',
             test_directories=None,
             write_directories=None,
-            ignore_parameter_names=('self','cls'),
+            ignore_parameter_names=('self', 'cls'),
 
     ):
         """
         :param file_path: call this always with __file__
         """
-        self.top_level_directory = top_level_directory
-        self.test_directories = test_directories if test_directories else top_level_directory
-        self.write_directories = write_directories if write_directories else top_level_directory
+        self._path = os.path.split(file_path)[0]
+        self.top_level_directory = self.in_full(top_level_directory)
+        self.test_directories = self.dirlist(test_directories)
+        self.write_directories = self.dirlist(write_directories)
         self.ignore_parameter_names = ignore_parameter_names
-        self.config_path = os.path.split(file_path)[0]
+
+    def in_full(self, path_particle):
+        return os.path.join(self._path, path_particle)
+
+    def dirlist(self, list_of_tuples):
+        if list_of_tuples:
+            return [self.in_full(os.path.join(contained)) for contained in list_of_tuples]
+        else:
+            return [self.top_level_directory]
+
+
+config = DucktestConfiguration(__file__, top_level_directory='a', test_directories=['a'])
+print config.top_level_directory
+print config.test_directories
+print config.write_directories
+print config.ignore_parameter_names
