6e9240c75638683b686ffa104d74d413f925cf68
Markus Wissinger <markus.wissinger@gmail.com>
2017-01-02 18:42:07 +0000

    supertypes

--- ducktest/supertypes.py
+++ ducktest/supertypes.py
@@ -1,42 +1,22 @@
-from abc import abstractmethod
-
-from ducktest.base import chain, Processor, PlainTypeWrapper
+from ducktest.base import PlainTypeWrapper, ContainerTypeWrapper
 
 
 def is_true_subclass_of(a, b):
     return issubclass(a, b) and not issubclass(b, a)
 
 
-class TypeFilter(Processor):
-    wrapper = None
-
-    def process(self, a, b):
-        if type(a) == self.wrapper and type(b) == self.wrapper:
-            return self._process(a, b)
-        else:
-            return self.next_processor.process(a, b)
-
-    @abstractmethod
-    def _process(self, a, b):
-        pass
+def is_plain_subtype(a, b):
+    return isinstance(a, PlainTypeWrapper) and isinstance(b, PlainTypeWrapper) and is_true_subclass_of(a.own_type,
+                                                                                                       b.own_type)
 
 
-class PlainTypeFilter(TypeFilter):
-    wrapper = PlainTypeWrapper
-
-    def _process(self, a, b):
-        return not is_true_subclass_of(a.own_type, b.own_type)
+def is_container_subtype(a, b):
+    return isinstance(a, ContainerTypeWrapper) and isinstance(b, ContainerTypeWrapper) and is_true_subclass_of(
+        a.own_type, b.own_type)
 
 
-class PassFilter(Processor):
-    def process(self, *args, **kwargs):
-        return True
-
-
-filter_processor = chain(
-    PlainTypeFilter(),
-    PassFilter(),
-)
+def is_subtype_of(a, b):
+    return is_plain_subtype(a, b)
 
 
 def remove_subtypes(some_types):
@@ -44,7 +24,7 @@ def remove_subtypes(some_types):
     for a in some_types:
         should_add = True
         for b in some_types:
-            if not filter_processor.process(a, b):
+            if is_subtype_of(a, b):
                 should_add = False
         if should_add:
             result.add(a)
