9c2994f3bbb7df3a35d0aeda49290790a424c58e
Markus Wissinger <mwissinger@andrena.de>
2017-04-20 16:49:50 +0200

    fix test

--- tests/test_configuration.py
+++ tests/test_configuration.py
@@ -1,3 +1,4 @@
+import os
 import unittest
 
 from hamcrest import assert_that, is_
@@ -13,9 +14,9 @@ class TestConfiguration(unittest.TestCase):
             test_directories=['some/path/test'.split('/')],
             write_directories=['some/path/src'.split('/'), 'some/path/write'.split('/')],
         )
-        assert_that(config.top_level_directory, is_('/home/user/some/path'))
-        assert_that(config.discover_tests_in_directories, is_(['/home/user/some/path/test']))
+        assert_that(config.top_level_directory, is_(os.path.join('home', 'user', 'some', 'path')))
+        assert_that(config.discover_tests_in_directories, is_([os.path.join('home', 'user', 'some', 'path', 'test')]))
         assert_that(config.write_docstrings_in_directories, is_([
-            '/home/user/some/path/src',
-            '/home/user/some/path/write',
+            os.path.join('home', 'user', 'some', 'path', 'src'),
+            os.path.join('home', 'user', 'some', 'path', 'write'),
         ]))
