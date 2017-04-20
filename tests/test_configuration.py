f51c47958844e0945045bfc5f7753dcbc6312ee7
markus.wissinger <markus.wissinger@andrena.de>
2017-04-20 17:00:09 +0200

    fix test

--- tests/test_configuration.py
+++ tests/test_configuration.py
@@ -1,6 +1,5 @@
 import os
 import unittest
-
 from hamcrest import assert_that, is_
 
 from ducktest.configuration import DucktestConfiguration
@@ -9,14 +8,17 @@ from ducktest.configuration import DucktestConfiguration
 class TestConfiguration(unittest.TestCase):
     def test_creation(self):
         config = DucktestConfiguration(
-            '/home/user/',
+            __file__,
             '/some/path'.split('/'),
             test_directories=['some/path/test'.split('/')],
             write_directories=['some/path/src'.split('/'), 'some/path/write'.split('/')],
         )
-        assert_that(config.top_level_directory, is_(os.path.join('home', 'user', 'some', 'path')))
-        assert_that(config.discover_tests_in_directories, is_([os.path.join('home', 'user', 'some', 'path', 'test')]))
+
+        path = os.path.split(__file__)[0]
+
+        assert_that(config.top_level_directory, is_(os.path.join(path, 'some', 'path')))
+        assert_that(config.discover_tests_in_directories, is_([os.path.join(path, 'some', 'path', 'test')]))
         assert_that(config.write_docstrings_in_directories, is_([
-            os.path.join('home', 'user', 'some', 'path', 'src'),
-            os.path.join('home', 'user', 'some', 'path', 'write'),
+            os.path.join(path, 'some', 'path', 'src'),
+            os.path.join(path, 'some', 'path', 'write'),
         ]))
